922badcd2845ed6d920843d56347f0bc18422234
M Abdillah Hafizh <sudesgi10@gmail.com>
2024-12-15 21:14:13 +0700

    tambah h2

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>blue2</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur labore odio perferendis quas voluptate rem ipsa a cumque nesciunt, sit repellendus aperiam iste numquam, ipsum distinctio reprehenderit minus laudantium expedita.</p>
+    <h2>yoo</h2>
 </body>
 </html>
