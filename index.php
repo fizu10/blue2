c5adc3bdad4446033868bca720391990cc39ec01
M Abdillah Hafizh <sudesgi10@gmail.com>
2024-12-15 21:08:56 +0700

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blue2</title>
+</head>
+<body>
+    <h1>blue2</h1>
+</body>
+</html>
